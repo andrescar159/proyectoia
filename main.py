7fa42b23c133964b3143d00baddd27e4cd19173f
andrescar159 <andrescar159@hotmail.com>
2025-07-31 20:16:35 -0500

    Primer ejercicio con stopwords

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ from nltk.probability import FreqDist
 # Definimos un texto en español que queramos analizar
 
 texto = """
-¿Cómo funciona la IA?
-Las Inteligencias artificiales utilizan algoritmos y modelos matemáticos para procesar grandes cantidades de datos y tomar decisiones basadas en patrones y reglas establecidas a través del aprendizaje automático, que es la capacidad de una máquina para aprender de forma autónoma a partir de datos sin ser programada específicamente para hacerlo. De esta manera la IA puede mejorar su precisión y eficiencia con el tiempo.
-Espero que esta información sobre la IA sea de gran apoyo para su formación y aprendizaje.
+Mi nombre es Carlos Andres, me dedico a la programacion y me gusta mucho el futbol.
+Tambien me gusta mucho la musica y la lectura, tambien soy instructor de programacion.
+
+Mi compañero es Jheyson Galvis, soy profesor, me gusta caminar y viajar, dormir.
 """
 
 # Tokenización: Convertimos el texto en una lista de palabras individuales
